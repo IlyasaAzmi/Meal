0bc56676b73c63439a99dbd7db848615466370b3
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-10 16:30:33 +0700

    Initial Commit

---
+++ Meal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meal
+//
+//  Created by Ilyasa Azmi on 10/03/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
